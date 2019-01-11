cf13064fedf9bd689582c53ba0e54b9d4a0a3d74
Huixiang Qiao <369861826@qq.com>
2019-01-11 15:42:24 +0800

    Update 彩色图像颜色反转.py

--- Special effects in image processing/彩色图像颜色反转.py
+++ Special effects in image processing/彩色图像颜色反转.py
@@ -8,17 +8,12 @@ cv2.imshow('src', img)
 imgInformation = img.shape  
 height = imgInformation[0]  
 width = imgInformation[1]  
-deep = imgInformation[2]  
-newImgInformation = (height*2, width, deep)  
-dst = np.zeros(newImgInformation, np.uint8) 
+dst = np.zeros((height, width, 3), np.uint8)  # 3表示彩色图
 
-for i in range(0, height):  
+for i in range(0, height):
     for j in range(0, width):
-        dst[i, j] = img[i, j]  # 绘制原图片部分
-        dst[height*2-i-1, j] = img[i, j]  # 绘制镜像图片部分
+        (b, g, r) = img[i, j]  # 读取图像中b,g,r三种颜色
+        dst[i, j] = (255-b, 255-g, 255-r)  # 彩色图像颜色反转放于dst矩阵中
 		
-for i in range(0, width):  # 在镜像图片和原图片中间绘制分割线
-    dst[height, i] = (0, 0, 255)  # 绘制红线
-	
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
