2e2a624b49a9db1fa90b66a183ee03baa3cf4855
Huixiang Qiao <369861826@qq.com>
2019-01-11 15:45:23 +0800

    Update 灰度图像颜色反转.py

--- Special effects in image processing/灰度图像颜色反转.py
+++ Special effects in image processing/灰度图像颜色反转.py
@@ -8,17 +8,13 @@ cv2.imshow('src', img)
 imgInformation = img.shape  
 height = imgInformation[0]  
 width = imgInformation[1]  
-deep = imgInformation[2]  
-newImgInformation = (height*2, width, deep)  
-dst = np.zeros(newImgInformation, np.uint8) 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
+dst = np.zeros((height, width, 1), np.uint8)  # 1表示灰度图(单通道)
 
-for i in range(0, height):  
+for i in range(0, height):
     for j in range(0, width):
-        dst[i, j] = img[i, j]  # 绘制原图片部分
-        dst[height*2-i-1, j] = img[i, j]  # 绘制镜像图片部分
+        grayPixel = gray[i, j]  # 当前的每一个灰度值
+        dst[i, j] = 255 - grayPixel  # 颜色反转放于新的数组
 		
-for i in range(0, width):  # 在镜像图片和原图片中间绘制分割线
-    dst[height, i] = (0, 0, 255)  # 绘制红线
-	
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
