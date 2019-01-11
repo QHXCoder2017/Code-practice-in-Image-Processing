5463bc78c2d48a802d880750369330f1d4d6761d
Huixiang Qiao <369861826@qq.com>
2019-01-11 15:02:07 +0800

    Add files via upload

---
+++ Geometric transformation of image processing/图像移位.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+
+img = cv2.imread('naruto.jpg', 1)
+cv2.imshow('src', img)
+imgInformation = img.shape  
+height = imgInformation[0]  
+width = imgInformation[1]  
+dst = np.zeros(img.shape, np.uint8) 
+for i in range(0, height):  
+    for j in range(0, width-100):  # 向右移
+        dst[i, j+100] = img[i, j]
+		
+cv2.imshow('image', dst)
+cv2.waitKey(0)
