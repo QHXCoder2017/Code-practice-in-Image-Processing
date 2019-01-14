333249488f9ff41b21d5d97b3f357f509b551144
Huixiang Qiao <369861826@qq.com>
2019-01-14 16:19:03 +0800

    Update 灰度处理2.py

--- Special effects in image processing/灰度处理2.py
+++ Special effects in image processing/灰度处理2.py
@@ -8,17 +8,16 @@ cv2.imshow('src', img)
 imgInformation = img.shape  
 height = imgInformation[0]  
 width = imgInformation[1]  
-deep = imgInformation[2]  
-newImgInformation = (height*2, width, deep)  
-dst = np.zeros(newImgInformation, np.uint8) 
-
+mode = imgInformation[2]  # 当前每个像素由多少个颜色组成
+dst = np.zeros((height, width, 3), np.uint8)  
 for i in range(0, height):  
     for j in range(0, width):
-        dst[i, j] = img[i, j]  # 绘制原图片部分
-        dst[height*2-i-1, j] = img[i, j]  # 绘制镜像图片部分
-		
-for i in range(0, width):  # 在镜像图片和原图片中间绘制分割线
-    dst[height, i] = (0, 0, 255)  # 绘制红线
+        (b, g, r) = img[i, j]  # 读取原图的像素点上的像素值BGR
+        b = int(b)
+	g = int(g)
+        r = int(r)
+        gray = r*0.299 + g*0.587 + b*0.114
+        dst[i, j] = np.uint8(gray)
 	
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
